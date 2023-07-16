e70c605a70c0fb30fbdb09594fe6a8be7732b193
talda24-meet <139680993+talda24-meet@users.noreply.github.com>
2023-07-16 18:57:33 +0300

    tal dahan lab1
    i am unfinished

---
+++ lab1.py
@@ -0,0 +1,17 @@
+def create_youtube_video(title, description):
+	youtube_video = {"title" : title, "description" : description, "likes" : 0, "dislikes" : 0, "comments" : comments{username : comment_text}}
+	return youtube_video
+
+def like(youtube_video):
+	if "likes" in youtube_video:
+		"likes" += 1
+
+def dislike(youtube_video):
+	if "dislikes" in youtube_video:
+		"dislikes" += 1
+
+def add_comment(youtube_video, username, comment_text)
+	comments = {"username": "here is my comment"}
+	return comments
+
+new_video = create_youtube_video("my video", "this is my video")
