c536b42fc7da3b986f0158b91ce7e160538410f1
talda24-meet <139680993+talda24-meet@users.noreply.github.com>
2023-07-17 23:48:02 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,17 +1,27 @@
-def create_youtube_video(title, description):
-	youtube_video = {"title" : title, "description" : description, "likes" : 0, "dislikes" : 0, "comments" : comments{username : comment_text}}
+def create_youtube_video():
+	title = input("Enter the title: ")
+	description = input("Enter the description: ")
+	youtube_video = {"title" : title, "description" : description, "likes" : 0, "dislikes" : 0, "comments" : {}}
 	return youtube_video
 
 def like(youtube_video):
 	if "likes" in youtube_video:
-		"likes" += 1
+		youtube_video["likes"] += 1
+	return youtube_video
 
 def dislike(youtube_video):
 	if "dislikes" in youtube_video:
-		"dislikes" += 1
+		youtube_video["dislikes"] += 1
+	return youtube_video
 
-def add_comment(youtube_video, username, comment_text)
-	comments = {"username": "here is my comment"}
-	return comments
+def add_comment(youtube_video, username, comment_text):
+	youtube_video["comments"][username] = comment_text
+	return youtube_video
 
-new_video = create_youtube_video("my video", "this is my video")
+youtube_video = create_youtube_video()
+like(youtube_video)
+dislike(youtube_video)
+username = input("Enter your username: ")
+comment_text = input("Enter your comment: ")
+add_comment(youtube_video, username, comment_text)
+print(youtube_video)
